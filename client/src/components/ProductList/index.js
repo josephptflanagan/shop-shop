1577b8b1f6a861819faba00ec4d8163dd620ed93
Joe Flanagan <josephptflanagan@gmail.com>
2020-10-15 13:25:50 -0500

    added index.db for offline functionality

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -4,6 +4,8 @@ import { useQuery } from '@apollo/react-hooks';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 
+import { idbPromise } from "../../utils/helpers";
+
 import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
@@ -34,8 +36,22 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });// add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
